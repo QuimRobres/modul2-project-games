81b453e6143677ae4bcf1453beefa4470eb20cc0
QuimRobres <quim.robres@gmail.com>
2021-04-23 12:59:43 +0200

    fixed user model

--- models/User.model.js
+++ models/User.model.js
@@ -3,8 +3,10 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
     username: { type: String, required: true },
-    email: { type: Email, required: true },
-    password: { type: String, required: true}
+    email: { type: String, required: true },
+    password: { type: String, required: true},
+    owned_games: [String],
+    wishlist: [String]
 })
 
 const User = mongoose.model("User", userSchema);
