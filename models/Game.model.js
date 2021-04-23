3711e392f9611997d47c902846ebc70fee22be68
l-masip <laia.masip.a@gmail.com>
2021-04-23 12:50:56 +0200

    game model

---
+++ models/Game.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+    name: { type: String, required: true, unique: true },
+    year_published: { type: Number },
+    min_players: { type: Number },
+    max_players: { type: Number },
+    min_playtime: { type: Number },
+    max_playtime: { type: Number },
+    min_age: { type: Number },
+    description: { type: String },
+    image_url: { type: String },
+    price: { type: Number },
+    rating: { type: Number, min: 0, max: 5}
+})
+
+const Game = mongoose.model('games', gameSchema);
+
+module.exports = Game;
