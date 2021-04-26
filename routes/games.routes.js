d5dd70399988a4cd2fa2b35cbdeed687ed7cbde4
QuimRobres <quim.robres@gmail.com>
2021-04-26 15:05:21 +0200

    some game routes done

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -3,11 +3,21 @@ const router = require(".");
 const Game = require('../models/Game.model')
 
 router.get('/search-game-result', (req, res) => {
-    Game.find({min_age: {$gte: 5}})
-    .then(games => {
-        res.render("public/search-game-result", {games})
-    })
-    .catch((error) => console.error(error))
+
+    if("number"){
+        Game.find({min_age: {$gte: 5}})
+        .then(games => {
+            res.render("public/search-game-result", {games})
+        })
+        .catch((error) => console.error(error))
+    } else if ("age") {
+        Game.find({average_user_rating: {$gte: 3}})
+        .then(games => {
+            res.render("public/search-game-result", {games})
+        })
+        .catch((error) => console.error(error))
+    }
+   
 })
 
 module.exports = router;
